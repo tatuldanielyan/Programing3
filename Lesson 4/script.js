3957b7d279521fb411d09543ca1e3fbd54c9cb6c
tatuldanielyan <tatul.danielyan.s@tumo.org>
2019-10-15 21:20:17 +0400

    Project

--- Lesson 4/script.js
+++ Lesson 4/script.js
@@ -1,15 +1,26 @@
-
 function main() {
-   var socket = io();
-   var chatDiv = document.getElementById('chat');
-   var input = document.getElementById('message');
-   var button = document.getElementById('submit');
-
-   function handleSubmit(evt) {
-       var val = input.value;
-       if (val != "") {
-           socket.emit("send message", val);
-       }
-   }
-   button.onclick = handleSubmit;
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+ 
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+ 
+    
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
 }
+
+socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main; 
